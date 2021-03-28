8542c8153335f128577b820f2cd6ef4d9f0657cd
zeevolution <limajosent@gmail.com>
2021-03-28 16:41:25 -0300

    feat: crawler output

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/json/CrawlResultWriter.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/json/CrawlResultWriter.java
@@ -1,6 +1,11 @@
 package com.udacity.webcrawler.json;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
 import java.io.Writer;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Objects;
 
@@ -25,10 +30,13 @@ public final class CrawlResultWriter {
    *
    * @param path the file path where the crawl result data should be written.
    */
-  public void write(Path path) {
+  public void write(Path path) throws IOException {
     // This is here to get rid of the unused variable warning.
     Objects.requireNonNull(path);
-    // TODO: Fill in this method.
+
+    try (Writer writer = Files.newBufferedWriter(path)) {
+      write(writer);
+    }
   }
 
   /**
@@ -36,9 +44,12 @@ public final class CrawlResultWriter {
    *
    * @param writer the destination where the crawl result data should be written.
    */
-  public void write(Writer writer) {
+  public void write(Writer writer) throws IOException {
     // This is here to get rid of the unused variable warning.
     Objects.requireNonNull(writer);
-    // TODO: Fill in this method.
+
+    final ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.disable(JsonGenerator.Feature.AUTO_CLOSE_TARGET);
+    objectMapper.writeValue(writer, this.result);
   }
 }
